81ac0de64d337864bf711dc0c73fc88fca3b4c4d
Christopher Lau <christopherjnlau@gmail.com>
2018-09-18 13:41:19 -1000

    Finished exercise

---
+++ fiberoo.js
@@ -0,0 +1,29 @@
+function fib() {
+  let first = 1;
+  let sec = 1;
+  let third = 0;
+  const array = [];
+  array.push(third);
+  for (let i = 0; i < 99; i++) {
+    sec = first;
+    first = third;
+    third = first + sec;
+    array.push(third);
+  }
+  return array;
+}
+
+console.log(fib());
+
+function numToStrings(data) {
+  return _.map(data, function (num) { return num.toString(); });
+}
+
+console.log(numToStrings(fib()));
+
+function evenNums(data) {
+  const a = _.filter(data, function (num) { return num % 2 === 0; });
+  return _.size(a);
+}
+
+console.log(evenNums(fib()));
